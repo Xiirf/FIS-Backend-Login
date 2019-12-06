c9fa313be2017277649dafe458bb4914885d884c
Flavien Cocu <Flavien.cocu@insa-rouen.fr>
2019-12-06 22:27:33 +0100

    Atlas

--- db/index.js
+++ db/index.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
+const uri = "mongodb://Xiirf:testmongo@cluster0-shard-00-00-ri07w.mongodb.net:27017,cluster0-shard-00-01-ri07w.mongodb.net:27017,cluster0-shard-00-02-ri07w.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority";
 
 mongoose
-    .connect('mongodb://localhost:27017/TEST', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+    .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
     .catch(e => {
         console.error('Connection error', e.message)
     })
