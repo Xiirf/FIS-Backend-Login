4aea96833bbadb61ba1c4a7c77f6cba772d6e2fc
Flavien Cocu <Flavien.cocu@insa-rouen.fr>
2019-12-06 18:22:26 +0100

    debug

--- tests/server.test.js
+++ tests/server.test.js
@@ -53,7 +53,7 @@ describe('Test with 2 users mock', () => {
                 password: "passwordTest1",
             })
             .end((err, response) => {
-                console.log("REP " + response);
+                console.log("REP " + JSON.stringify(response));
                 console.log("EREUR" + err);
                 token = response.body.token; // save the token!
                 console.log(token);
@@ -67,7 +67,7 @@ describe('Test with 2 users mock', () => {
     
 
     describe('GET /users', () => {
-        it('It should return all users', () => {
+        it('It should return all users', async () => {
             console.log(token);
             return request(app)
                     .get("/api/v1/users")
