99b49376429ef42503859395aa26964ac9f9a528
Flavien Cocu <Flavien.cocu@insa-rouen.fr>
2019-12-06 23:12:32 +0100

    define port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 const db = require('./db');
-const apiPort = 6201;
+const apiPort = (process.env.PORT || 6201);
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`));
