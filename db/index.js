5e3eaeedea3906fcc05e13a4151f4d5d054e3b4b
Flavien Cocu <Flavien.cocu@insa-rouen.fr>
2019-12-13 15:04:33 +0100

    Debug test without env var

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 console.log('URI' + JSON.stringify(process.env))
 mongoose
-    .connect(process.env.uriDataBase, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+    .connect("mongodb://Xiirf:testmongo@cluster0-shard-00-00-ri07w.mongodb.net:27017,cluster0-shard-00-01-ri07w.mongodb.net:27017,cluster0-shard-00-02-ri07w.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
     .catch(e => {
         console.error('Connection error', e.message)
     })
